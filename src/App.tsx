296de994000aafca4d8434e077427f78f412e825
miniusaco <80330292+miniusaco@users.noreply.github.com>
2022-05-25 23:14:00 +0900

    61.カスタムフックを使わないデータ取得例

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,53 @@
 import "./styles.css";
 import { UserCard } from "./components/UserCard";
-
-const user = {
-  id: 1,
-  name: "いぬこ",
-  email: "1213@aaa.com",
-  address: "ADDRESS"
-};
+import axios from "axios";
+import { User } from "./types/api/user";
+import { useState } from "react";
+import { UserProfile } from "./types/userProfile";
 
 export default function App() {
+  const [userProfiles, setuserProfiles] = useState<Array<UserProfile>>([]);
+  const [loding, setLoding] = useState(false);
+  const [error, setError] = useState(false);
+
+  const onClickFetchUser = () => {
+    setLoding(true);
+    setError(false);
+
+    axios
+      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        const data = res.data.map((user) => ({
+          id: user.id,
+          name: `${user.name}(${user.username})`,
+          email: user.email,
+          address: `${user.address.city}${user.address.suite}${user.address.street}`
+        }));
+        setuserProfiles(data);
+      })
+      .catch(() => {
+        setError(true);
+      })
+      .finally(() => {
+        setLoding(false);
+      });
+  };
+
   return (
     <div className="App">
-      <UserCard user={user} />
+      <button onClick={onClickFetchUser}>データ取得</button>
+      <br />
+      {error ? (
+        <p style={{ color: "red" }}>データの取得に失敗しました</p>
+      ) : loding ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          {userProfiles.map((user) => (
+            <UserCard key={user.id} user={user} />
+          ))}
+        </>
+      )}
     </div>
   );
 }
--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -5,21 +5,21 @@ type Props = {
   user: UserProfile;
 };
 
-export const UserCard： VFC<Props> = (props) => {
+export const UserCard: VFC<Props> = (props) => {
   const { user } = props;
 
-const style = {
-  border: "solid 1px #ccc",
-  borderRadius: "8px",
-  padding: "0 16px",
-  margin: "8px"
-};
+  const style = {
+    border: "solid 1px #ccc",
+    borderRadius: "8px",
+    padding: "0 16px",
+    margin: "8px"
+  };
 
   return (
     <div style={style}>
       <dl>
         <dt>名前</dt>
-         <dd>{user.name}</dd>
+        <dd>{user.name}</dd>
         <dt>メール</dt>
         <dd>{user.email}</dd>
         <dt>住所</dt>
