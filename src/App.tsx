d12dc64f32eadceffd2edb017f1d6eed810771ab
miniusaco <80330292+miniusaco@users.noreply.github.com>
2022-05-26 08:58:54 +0900

    62.カスタムフックを呼び出す

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,11 @@
 import "./styles.css";
 import { UserCard } from "./components/UserCard";
-import axios from "axios";
-import { User } from "./types/api/user";
-import { useState } from "react";
-import { UserProfile } from "./types/userProfile";
+import { useAllUsers } from "./hooks/useAllUsers";
 
 export default function App() {
-  const [userProfiles, setuserProfiles] = useState<Array<UserProfile>>([]);
-  const [loding, setLoding] = useState(false);
-  const [error, setError] = useState(false);
+  const { getUsers, userProfiles, loding, error } = useAllUsers();
 
-  const onClickFetchUser = () => {
-    setLoding(true);
-    setError(false);
-
-    axios
-      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
-      .then((res) => {
-        const data = res.data.map((user) => ({
-          id: user.id,
-          name: `${user.name}(${user.username})`,
-          email: user.email,
-          address: `${user.address.city}${user.address.suite}${user.address.street}`
-        }));
-        setuserProfiles(data);
-      })
-      .catch(() => {
-        setError(true);
-      })
-      .finally(() => {
-        setLoding(false);
-      });
-  };
+  const onClickFetchUser = () => getUsers();
 
   return (
     <div className="App">
